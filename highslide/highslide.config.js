0e27e21ee25260059dd5f6a7ed64803460a74b2f
Badwithmyhands <70039527+Badwithmyhands@users.noreply.github.com>
2020-08-22 19:33:36 -0400

    Test website

---
+++ highslide/highslide.config.js
@@ -0,0 +1,54 @@
+/**
+*	Site-specific configuration settings for Highslide JS
+*/
+hs.graphicsDir = 'highslide/graphics/';
+hs.showCredits = false;
+hs.creditsPosition = 'bottom left';
+hs.outlineType = 'custom';
+hs.dimmingOpacity = 0.5;
+hs.fadeInOut = true;
+hs.align = 'center';
+hs.padToMinWidth = true;
+hs.useBox = true;
+hs.width = 600;
+hs.height = 400;
+hs.allowMultipleInstances = false;
+hs.captionEval = 'this.none';
+hs.headingEval = 'this.thumb.title';
+hs.registerOverlay({
+	html: '<div class="closebutton" onclick="return hs.close(this)" title="Close Gallery"></div>',
+	position: 'top right',
+	useOnHtml: true,
+	fade: 2 // fading the semi-transparent overlay looks bad in IE
+});
+
+
+
+// Add the slideshow controller
+hs.addSlideshow({
+	slideshowGroup: 'group1',
+	interval: 5000,
+	repeat: false,
+	useControls: true,
+	overlayOptions: {
+		className: 'controls-in-heading',
+		opacity: '1.00',
+		position: 'bottom left',
+		offsetX: '0',
+		offsetY: '0',
+		hideOnMouseOut: false
+	},
+	thumbstrip: {
+		mode: 'vertical',
+		position: 'rightpanel',
+		relativeTo: 'image'
+	}
+
+});
+
+// gallery config object
+var config1 = {
+	slideshowGroup: 'group1',
+	thumbnailId: 'thumb1',
+	transitions: ['expand', 'crossfade']
+};
---
+++ oldsite-sg.org/newindex.php
@@ -0,0 +1,112 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html><head><title>Southern Gardening: What do you want to grow today?</title>
+
+
+<meta name="robots" content="index,follow">
+<meta name="distribution" content="Global">
+<meta name="revisit-after" content="4 days">
+<meta name="rating" content="General">
+<meta name="Abstract" content="Southern Gardening">
+<meta name="Copyright" content="© 2010 Sourthern Gardening">
+<meta name="Author" content="http://www.southerngardening.org">
+<meta name="host" content="http://www.southerngardening.org">
+<meta name="root" content="http://www.southerngardening.org">
+<meta name="page-topic" content="southern gardening, native plants, landscaping, Gainesville">
+<meta name="page-type" content="southern gardening, native plants, landscaping, Gainesville">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="keywords" content="garden, gardening, southern gardening, florida gardening, regional gardening, nature gardening, groundcover landscape, landscaping, native plants, flowers, vegetables, gainesville, lawn, yard, xeriscape, xeriscaping">
+<meta name="description" content="Southern Gardening provides in-depth articles and weekly news and tips on gardening in the Coastal South region of the U.S., focusing on North Central Florida."></head><body style="color: rgb(0, 0, 0); font-family: Helvetica,Arial,sans-serif; background-color: rgb(255, 255, 255);" alink="#ff3333" link="#000099" vlink="#990099">
+<table style="font-family: Helvetica,Arial,sans-serif;" cellpadding="20">
+<tbody>
+<tr>
+<td style="width: 180px; vertical-align: top; background-color: #FFFF99;">
+<table style="text-align: left; width: 100%;" border="0" cellpadding="0" cellspacing="0">
+<tbody>
+<tr>
+<td style="vertical-align: top;"> <? readfile("menu.html"); ?>
+            <br>
+</td>
+</tr>
+</tbody>
+</table>
+</td><td style="width: 627px; vertical-align: top; background-color: white;">
+<big style="font-weight: bold; color: rgb(153, 0, 0);"><big><img style="width: 152px; height: 256px;" alt="" src="http://www.southerngardening.org/dbn/images/hidden2.jpg" align="right" hspace="20" vspace="5">SouthernGardening.org</big></big><br><big style="font-weight: bold; font-style: italic; color: rgb(153, 0, 0);">What
+do you want to grow today?</big><br><br>
+A Guide to Southern Gardening by Jim Notestein<br>
+<br>
+<small style="color: rgb(153, 0, 0);"><b>What do we mean by "southern gardening"</b></small><small><b>?<br>
+<br>
+</b></small> <small>We
+mean the region known to gardeners as the Coastal South, stretching
+from the Gulf coast of Texas to the Atlantic coast of South Carolina.
+The region as a whole shares a number of distinctive characteristics:
+long, hot, humid summers; mild winters; abundant rain and sunshine
+(usually); many native plants; and best of all, the opportunity for
+year-round gardening. <br>
+</small> <br>
+<small>We
+focus on North Central Florida because
+that's where we are, and because it makes a good microcosm of the
+region as a whole. <br>
+<br>
+</small> <small style="color: rgb(153, 0, 0);"> <b>Why do southern gardeners
+need a special
+website?</b></small><br>
+<br><small>
+Because
+gardening here is different. We're in the transition zone
+between temperate and tropical climates. Advice you get from gardening
+books and instructions on seed packets may give great results in Ohio,
+but produce only weeds and frustration here. A plant that needs full
+summer sun in Michigan may look like it's been toasted if it gets full
+summer sun in Florida. Our mild winters don't kill off bugs and weeds
+the way northern winters do, and so soil preparation and planting
+techniques that work up north can yield a bumper crop of aggravation
+here. In other words, we need a site that tells us how to grow where
+we're planted. </small> <br>
+<small> </small>
+<dl>
+<small> </small>
+</dl>
+<small style="color: rgb(153, 0, 0);"><b>How do we know all this?</b><br>
+<br>
+</small> <small>Jim
+Notestein is a fifth generation Floridian, and local grower of nearly
+40 years. He and his wife Emily manage the Plants4Life nursery in
+Gainesville and the &#8216;Agave Land&#8217; nursery in LaCrosse. Their
+horticultural products include North Florida native plants, edible
+plants, cacti and succulents, bonsai, and rare plants and trees from
+Florida and beyond. Jim is experienced in the propagation,
+installation, &amp; care of over 200 plant species, with a strong
+emphasis on Florida native plants. He has facilitated hundreds of small
+family food gardens in this area. Jim has also managed landscapes for
+pollination, pests, birds, butterflies, colors, fragrance, fire,
+heirlooms, lawns and lawn alternatives, restoration,light and shade,
+privacy and security, wildlife, water use and special sounds. He has
+designed and installed theme gardens, secret, sacred and xeric gardens,
+memory gardens, labyrinths and mazes, container gardens and coastal
+landscapes. He has done community food networks, contract growing,
+horticultural therapy, firewise certification, garden coaching, solar
+powered air conditioning, landscape structures, specimen plants and
+topiary art, water gardens and yard bells.<br>
+<br>
+<span style="font-style: italic; font-weight: bold; color: rgb(153, 0, 0);">
+<a href="http://www.southerngardening.org/dbn/plants.htm">Notestein's Nursery plants</a> are available retail, wholesale or contract-grown at:<br></span>
+<br>
+<span style="font-weight: bold;">Notestein's Nursery</span><br>
+3701 NW 17th Street<br>
+Gainesville, FL 32605<br><br />
+</small> <small><span style="font-weight: bold;">By Appointment Only<br />352-372-2107</span><br>
+<br>
+</small> 
+</small> <small><span style="font-style: italic; font-weight: bold; color: rgb(153, 0, 0);">Purchase
+online at </span><a style="font-style: italic; font-weight: bold; color: rgb(153, 0, 0);" href="http://www.nfgrows.com/index.php?main_page=index&amp;cPath=11">www.nfgrows.com</a></small><br>
+
+<hr> <font size="-1">
+<center><small> Copyright © 2010 by Southern Gardening</small>
+</center>
+</font> </td>
+</tr>
+</tbody>
+</table>
+</body></html>
